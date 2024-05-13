100d5d1cb7fb4b0714fd6c1f61140a348dd6d38e
sstrinic <Stipan.Strinic.00@fesb.hr>
2024-05-13 12:19:55 +0200

    Removed http script & moving to xdp

--- ebpf-project/ebpf.py
+++ ebpf-project/ebpf.py
@@ -10,7 +10,7 @@ def run_script_in_terminal(script):
 
 if __name__ == "__main__":
     script_dir = "scripts/"
-    script_paths = ["users-command.py"]
+    script_paths = ["users-command.py", "xdp.py"]
     script_paths = [script_dir + x for x in script_paths]
 
     for path in script_paths:
